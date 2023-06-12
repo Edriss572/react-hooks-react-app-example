5f1db99031403758767585944e21037054798816
Edriss572 <edriss_miakhel@yahoo.com>
2023-06-12 16:44:26 +0300

    The assigment is done

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,12 @@ function App() {
     <div className="App">
       <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
       <p className="App-intro">
-        In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
+        <h1>In React apps</h1>
+          
+        <p>we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the
         HTML, and also allows us to add in components, which are separate,
-        self-contained chunks of JSX.
+        self-contained chunks of JSX.</p>
       </p>
       <ExampleComponent />
     </div>
